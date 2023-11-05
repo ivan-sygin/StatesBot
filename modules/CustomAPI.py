af7b027c1adaa275559ecd88a40889091be45d6c
VLS_PC <ivan.sygin@yandex.ru>
2023-11-06 00:17:55 +0300

    Add methods for CustomAPI.py

--- modules/CustomAPI.py
+++ modules/CustomAPI.py
@@ -22,8 +22,9 @@ class API_Handler:
     port = '8000'
     url = ''
     path_token = ''
+    auth_type = ''
 
-    def __init__(self, protocol, ip, port, path_token):
+    def __init__(self, protocol, ip, port, path_token=None):
         self.protocol = protocol
         self.ip = ip
         self.port = port
@@ -32,26 +33,32 @@ class API_Handler:
     def updateUrl(self):
         self.url = self.protocol + "://" + self.ip + ":" + self.port
 
-    def fetchGetJson(self, api_path: str, auth=True, data=None) -> dict:
+    def fetchGetJson(self, api_path: str, auth_type='none', data=None) -> dict:
         url = self.url + api_path
+        headers = {}
         params = data
 
-        if auth:
+        if auth_type == 'get':
             params['token'] = os.environ[self.path_token]
+        elif auth_type == 'bearer':
+            headers['Authorization'] = 'Bearer ' + os.environ[self.path_token]
 
-        response = requests.get(url, params=params)
+        response = requests.get(url, params=params, headers=headers)
 
         if response.status_code != 200:
             raise generateException(response.status_code, 'fetchGetJson', response.request)
 
         return response.json()
 
-    def fetchGetNoResponse(self, api_path: str, auth=True, data=None):
+    def fetchGetNoResponse(self, api_path: str, auth_type='none', data=None):
         url = self.url + api_path
+        headers = {}
         params = data
 
-        if auth:
+        if auth_type == 'get':
             params['token'] = os.environ[self.path_token]
+        elif auth_type == 'bearer':
+            headers['Authorization'] = 'Bearer ' + os.environ[self.path_token]
 
         response = requests.get(url, params=params)
 
@@ -59,3 +66,23 @@ class API_Handler:
             raise generateException(response.status_code, 'fetchGetNoResponse', response.request)
 
         return response.status_code
+
+    def fetchPostJson(self, api_path: str, auth_type='none', data=None) -> dict:
+        url = self.url + api_path
+        headers = {}
+        payload = {}
+        json = data
+
+        if auth_type == 'get':
+            payload['token'] = os.environ[self.path_token]
+        elif auth_type == 'json':
+            json['token'] = os.environ[self.path_token]
+        elif auth_type == 'bearer':
+            headers['Authorization'] = 'Bearer ' + os.environ[self.path_token]
+
+        response = requests.post(url, json=json, headers=headers, params=payload)
+
+        if response.status_code != 200:
+            raise generateException(response.status_code, 'fetchGetJson', response.request)
+
+        return response.json()
